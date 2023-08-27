f91a3ac93a98cc20865cb5a8ef39da9cd9d9e222
shmsacr <shmsacr@gmail.com>
2023-08-27 07:09:29 +0300

    Initial commit

---
+++ src/main/java/com/shmsacr/video_call_application/VideoCallApplication.java
@@ -0,0 +1,13 @@
+package com.shmsacr.video_call_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCallApplication.class, args);
+	}
+
+}
