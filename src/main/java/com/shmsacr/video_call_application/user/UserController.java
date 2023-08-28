a612cec024693ade28d2c1f508cd7584d56a50fa
shmsacr <shmsacr@gmail.com>
2023-08-28 18:33:57 +0300

    feat: add user controller

---
+++ src/main/java/com/shmsacr/video_call_application/user/UserController.java
@@ -0,0 +1,43 @@
+package com.shmsacr.video_call_application.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@Slf4j
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public void register(User user){
+        userService.register(user);
+    }
+    @PostMapping("/login")
+    public User login(@RequestBody User user){
+        return userService.login(user);
+    }
+    @PostMapping("/logout")
+    public void logout(@RequestBody User email){
+        userService.logout(email.getEmail());
+    }
+    @GetMapping
+    public  List<User> findAll() {
+        return userService.findAll();
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle(Exception ex){
+        ex.printStackTrace();
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
+    }
+}
