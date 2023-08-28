7caa05bdb876c34ddbda6befad9ecfc8dea65da5
shmsacr <shmsacr@gmail.com>
2023-08-28 18:34:30 +0300

    feat: add user

--- src/main/java/com/shmsacr/video_call_application/VideoCallApplication.java
+++ src/main/java/com/shmsacr/video_call_application/VideoCallApplication.java
@@ -1,7 +1,11 @@
 package com.shmsacr.video_call_application;
 
+import com.shmsacr.video_call_application.user.User;
+import com.shmsacr.video_call_application.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VideoCallApplication {
@@ -9,5 +13,29 @@ public class VideoCallApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VideoCallApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(UserService userService){
+		return args -> {
+			userService.register(User.builder()
+					.username("Sehmus")
+					.email("shmsacr@gmail.com")
+					.password("aaa")
+					.build());
+
+			userService.register(User.builder()
+					.username("Other")
+					.email("other@gmail.com")
+					.password("aaa")
+					.build());
+
+			userService.register(User.builder()
+					.username("Test")
+					.email("test@gmail.com")
+					.password("aaa")
+					.build());
+		};
+	}
+
 
 }
